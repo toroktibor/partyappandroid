5958da287b1f6de8fdb41214fbfa0ad2ff19eddd
vtms <vtamashun@gmail.com>
2014-01-09 19:04:19 +0100

    NonSwipeableViewPager added

---
+++ src/hu/schonherz/y2014/partyappandroid/NonSwipeableViewPager.java
@@ -0,0 +1,29 @@
+package hu.schonherz.y2014.partyappandroid;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NonSwipeableViewPager extends ViewPager {
+
+	public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public NonSwipeableViewPager(Context context) {
+		super(context);
+	}
+
+
+	@Override
+	public boolean onTouchEvent(MotionEvent arg0) {
+		return false;
+	}
+	
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent arg0) {
+		return false;
+	}
+
+}
