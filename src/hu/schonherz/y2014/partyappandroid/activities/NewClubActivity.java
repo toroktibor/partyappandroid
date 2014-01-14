e1b39b358f9e4851436021c47887a0205c075c09
Lamfalusy <Lamfalusy@Lamfalusy-PC>
2014-01-14 16:31:08 +0100

    Android klub hozzáadás funkció kész.

--- src/hu/schonherz/y2014/partyappandroid/activities/NewClubActivity.java
+++ src/hu/schonherz/y2014/partyappandroid/activities/NewClubActivity.java
@@ -1,8 +1,11 @@
 package hu.schonherz.y2014.partyappandroid.activities;
 
 import hu.schonherz.y2014.partyappandroid.R;
+import hu.schonherz.y2014.partyappandroid.util.datamodell.Session;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,11 +21,13 @@ public class NewClubActivity extends Activity {
 	Spinner newClubTypeSpinner;
 	CheckBox newClubOwnerCheckBox;
 	Button addButton;
+	Context thisContext;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_new_club);
+	thisContext = getApplicationContext();
 	
 	newClubNameEditText = (EditText) findViewById(R.id.new_club_edittext_name);
 	newClubAddressEditText = (EditText) findViewById(R.id.new_club_edittext_address);
@@ -37,12 +42,26 @@ public class NewClubActivity extends Activity {
 			
 			String newClubName = newClubNameEditText.getText().toString();
 			String newClubAddress = newClubAddressEditText.getText().toString();
-			//String newClubType = newClubTypeSpinner.getT
+			String newClubType = String.valueOf(newClubTypeSpinner.getSelectedItem());
+			Boolean isOwner = newClubOwnerCheckBox.isChecked();
 			
+			if(newClubName.isEmpty()){
+				Toast.makeText(thisContext, "Nem adtál meg nevet, ez baj, sűtgősed szedd össze magad.", Toast.LENGTH_LONG).show();
+				return;
+			}
+			
+			if(newClubAddress.isEmpty()){
+				Toast.makeText(thisContext, "Már arra se emlékszel hol basztál be tegnap? Szép vagy...", Toast.LENGTH_LONG).show();
+				return;
+			}
+			
+			int owner_user_id = isOwner ? Session.getInstance().getActualUser().id : -1;
+			
+			Session.getInstance().getActualCommunicationInterface().sendANewClubRequest(newClubName, newClubAddress, newClubType, owner_user_id);
+			
+			onBackPressed();
 		}
 	});
-	
-	Toast.makeText(this, "You fucked!", Toast.LENGTH_LONG).show();
 			
     }
 
--- src/hu/schonherz/y2014/partyappandroid/util/communication/CommunicationInterface.java
+++ src/hu/schonherz/y2014/partyappandroid/util/communication/CommunicationInterface.java
@@ -9,4 +9,7 @@ public interface CommunicationInterface {
 	public User authenticationUser(String nick_name, String password);
 	public List<Club> getFavoriteClubsFromUserId(int user_id);
 	public List<Club> getClubsFromCityName(String cityname);
+	
+	//Elküld egy új hely kérelmet a szervernek, ha a owner_user_id -1 akkor nincs neki tulaj jelöltje
+	public void sendANewClubRequest(String newClubName, String newClubAddress, String newClubType, int owner_user_id);
 }
