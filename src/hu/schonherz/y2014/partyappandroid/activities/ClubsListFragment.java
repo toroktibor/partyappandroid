e1c0a83a1f60a5650bec038cecba6cb1231b709b
vtms <vtamashun@gmail.com>
2014-01-14 16:50:25 +0100

    Sok-sok kikommentelt faszság...

--- src/hu/schonherz/y2014/partyappandroid/activities/ClubsListFragment.java
+++ src/hu/schonherz/y2014/partyappandroid/activities/ClubsListFragment.java
@@ -6,14 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class ClubsListFragment extends Fragment {
 
+	
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
     		Bundle savedInstanceState) {
     	ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_clubs_list, container, false);
+    	
+    	
+    	// ListView clubsListView = new ListView(getActivity().getApplicationContext());
+    	// rootView.addView(clubsListView);
     	return rootView;
     }
 
