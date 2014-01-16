fc1d8a0103512fe6eaa40e5c3b53f1958b951f79
toroktibor <trktiti@gmail.com>
2014-01-16 20:14:52 +0100

    Nemtudommicsodavanmostkérem. :D

--- src/hu/schonherz/y2014/partyappandroid/activities/ClubActivity.java
+++ src/hu/schonherz/y2014/partyappandroid/activities/ClubActivity.java
@@ -41,7 +41,7 @@ public class ClubActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_clubs);
+		setContentView(R.layout.activity_club);
 		
 		
 		ClubActionBar ab = new ClubActionBar(this);
--- src/hu/schonherz/y2014/partyappandroid/activities/PendingListActivity.java
+++ src/hu/schonherz/y2014/partyappandroid/activities/PendingListActivity.java
@@ -3,6 +3,7 @@ package hu.schonherz.y2014.partyappandroid.activities;
 import hu.schonherz.y2014.partyappandroid.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class PendingListActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class PendingListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_pending_list);
+
     }
 
 }
