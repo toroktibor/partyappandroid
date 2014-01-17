a2aadd08f85a8f40bef223f698d68574599a0033
Lamfalusy <Lamfalusy@Lamfalusy-PC>
2014-01-17 16:05:44 +0100

    Vannak események a Le'Programoz-Lak-ba.

--- src/hu/schonherz/y2014/partyappandroid/activities/ClubEventsFragment.java
+++ src/hu/schonherz/y2014/partyappandroid/activities/ClubEventsFragment.java
@@ -1,18 +1,39 @@
 package hu.schonherz.y2014.partyappandroid.activities;
 
+import java.util.List;
+
 import hu.schonherz.y2014.partyappandroid.R;
+import hu.schonherz.y2014.partyappandroid.adapters.EventsListAdapter;
+import hu.schonherz.y2014.partyappandroid.util.datamodell.Event;
+import hu.schonherz.y2014.partyappandroid.util.datamodell.Session;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class ClubEventsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 	ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_club_events, container, false);
+	
+	int clubListPosition = ClubActivity.intent.getExtras().getInt("listPosition");
+	ListView eventsListView = new ListView(getActivity().getApplicationContext());
+	Event[] eventArray = getEventArrayFromList(Session.getSearchViewClubs().get(clubListPosition).events);
+	eventsListView.setAdapter(new EventsListAdapter(getActivity(), eventArray));
+	
+	rootView.addView(eventsListView);
 	return rootView;
     }
-
+    
+    private Event[] getEventArrayFromList(List<Event> eventList){
+    	Event[] eventArray = new Event[eventList.size()];
+    	for(int i = 0; i< eventList.size(); ++i) {
+    		eventArray[i]=eventList.get(i);
+    	}
+    	return eventArray;
+    }
+    
 }
---
+++ src/hu/schonherz/y2014/partyappandroid/adapters/EventsListAdapter.java
@@ -0,0 +1,32 @@
+package hu.schonherz.y2014.partyappandroid.adapters;
+
+import hu.schonherz.y2014.partyappandroid.R;
+import hu.schonherz.y2014.partyappandroid.util.datamodell.Event;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class EventsListAdapter extends ArrayAdapter {
+    public EventsListAdapter(Context context, Event[] objects) {
+	super(context, R.layout.event_list_item, objects);
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+	View item = convertView;
+	if (item == null) {
+	    LayoutInflater inflater = ((Activity) getContext()).getLayoutInflater();
+	    item = inflater.inflate(R.layout.event_list_item, parent, false);
+	}
+	((TextView) item.findViewById(R.id.event_list_item_name)).setText(((Event) getItem(position)).name);
+	((TextView) item.findViewById(R.id.event_list_item_date)).setText(((Event) getItem(position)).start_date);
+	((TextView) item.findViewById(R.id.event_list_item_music_type)).setText(((Event) getItem(position)).music_type);
+	item.setTag((Event) getItem(position));
+	return item;
+    }
+}
