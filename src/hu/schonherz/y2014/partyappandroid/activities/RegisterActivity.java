90f3b909a895fd025e665e3b252f8c1e87b530d2
Lamfalusy <Lamfalusy@Lamfalusy-PC>
2014-01-17 15:24:33 +0100

    Regisztráció nem működik.

--- src/hu/schonherz/y2014/partyappandroid/activities/RegisterActivity.java
+++ src/hu/schonherz/y2014/partyappandroid/activities/RegisterActivity.java
@@ -5,28 +5,38 @@ import hu.schonherz.y2014.partyappandroid.dialogs.DatePickerFragment;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class RegisterActivity extends FragmentActivity {
 
+	EditText nameEditText;
+	EditText passwordEditText;
+	EditText password2EditText;
+	EditText emailEditText;
+	EditText dateOfBirthEditText;
+	Spinner spinnerSexSpinner;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_register);
-
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.activity_register);
+    	
     }
 
     public void onClickHandler(View v) {
-	if (v.getId() == R.id.register_button_register) {
+    	if (v.getId() == R.id.register_button_register) {
+    		
+    		finish();
+    	}
+    	if (v.getId() == R.id.register_edittext_dateofbirth) {
 
-	    finish();
-	}
-	if (v.getId() == R.id.register_edittext_dateofbirth) {
+    		DialogFragment datepicker = new DatePickerFragment();
+    		datepicker.show(getSupportFragmentManager(), "timePicker");
 
-	    DialogFragment datepicker = new DatePickerFragment();
-	    datepicker.show(getSupportFragmentManager(), "timePicker");
-
-	}
+    	}
     }
 
 }
