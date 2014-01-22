10e41b0f87d0a1ad3412a7fb677f24c2b24c71df
kapizoli <kapi.zoli@freemail.hu>
2014-01-22 17:35:10 +0100

    zoomolható is lesz a kép :D

--- src/hu/schonherz/y2014/partyappandroid/activities/FullImageActivity.java
+++ src/hu/schonherz/y2014/partyappandroid/activities/FullImageActivity.java
@@ -1,18 +1,32 @@
 package hu.schonherz.y2014.partyappandroid.activities;
 
-import hu.schonherz.y2014.partyappandroid.adapters.ImageItem;
 import hu.schonherz.y2014.partyappandroid.R;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.FloatMath;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.widget.ImageView;
 
 public class FullImageActivity extends Activity {
-
+	ImageView imageView;
+	Matrix matrix = new Matrix();
+	Matrix savedMatrix = new Matrix();
+	PointF startPoint = new PointF();
+	PointF midPoint = new PointF();
+	float oldDist = 1f;
+	static final int NONE = 0;
+	static final int DRAG = 1;
+	static final int ZOOM = 2;
+	int mode = NONE;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,6 +38,64 @@ public class FullImageActivity extends Activity {
         byte[] byteArray = getIntent().getByteArrayExtra("image");
         Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
         imageView.setImageBitmap(bmp);
+        imageView.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				ImageView view = (ImageView) v;
+				System.out.println("matrix=" + savedMatrix.toString());
+				switch (event.getAction() & MotionEvent.ACTION_MASK) {
+				case MotionEvent.ACTION_DOWN:
+					savedMatrix.set(matrix);
+					startPoint.set(event.getX(), event.getY());
+					mode = DRAG;
+					break;
+				case MotionEvent.ACTION_POINTER_DOWN:
+					oldDist = spacing(event);
+					if (oldDist > 10f) {
+						savedMatrix.set(matrix);
+						midPoint(midPoint, event);
+						mode = ZOOM;
+					}
+					break;
+				case MotionEvent.ACTION_UP:
+				case MotionEvent.ACTION_POINTER_UP:
+					mode = NONE;
+					break;
+				case MotionEvent.ACTION_MOVE:
+					if (mode == DRAG) {
+						matrix.set(savedMatrix);
+						matrix.postTranslate(event.getX() - startPoint.x,
+								event.getY() - startPoint.y);
+					} else if (mode == ZOOM) {
+						float newDist = spacing(event);
+						if (newDist > 10f) {
+							matrix.set(savedMatrix);
+							float scale = newDist / oldDist;
+							matrix.postScale(scale, scale, midPoint.x,
+									midPoint.y);
+						}
+					}
+					break;
+				}
+				view.setImageMatrix(matrix);
+				return true;
+			}
+
+			@SuppressLint("FloatMath")
+			private float spacing(MotionEvent event) {
+				float x = event.getX(0) - event.getX(1);
+				float y = event.getY(0) - event.getY(1);
+				return FloatMath.sqrt(x * x + y * y);
+			}
+
+			private void midPoint(PointF point, MotionEvent event) {
+				float x = event.getX(0) + event.getX(1);
+				float y = event.getY(0) + event.getY(1);
+				point.set(x / 2, y / 2);
+			}
+
+		});
 	}
 
 }
