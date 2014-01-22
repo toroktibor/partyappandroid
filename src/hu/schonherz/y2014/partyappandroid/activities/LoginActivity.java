373c956481cc8bbc73e75f92d312fb0ba2b06a40
kapizoli <kapi.zoli@freemail.hu>
2014-01-22 16:29:58 +0100

    loginnál lokációs bug javítva, kommentek kiszedve

--- src/hu/schonherz/y2014/partyappandroid/activities/LoginActivity.java
+++ src/hu/schonherz/y2014/partyappandroid/activities/LoginActivity.java
@@ -24,16 +24,9 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
-//import com.google.android.gms.common.ConnectionResult;
 
 public class LoginActivity extends Activity
-/*
- * implements GooglePlayServicesClient.ConnectionCallbacks,
- * GooglePlayServicesClient.OnConnectionFailedListener
- */{
-
-	// private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-	// LocationClient mLocationClient;
+{
 
 	boolean mBounded;
 	GPSLocation mGpsLocation;
@@ -70,9 +63,6 @@ public class LoginActivity extends Activity
 				}
 			}
 			// once done return true
-			if (isCancelled())
-				Log.e("async", "async task is cancelled");
-			Log.e("async", "isCancelled: " + isCancelled());
 			return true;
 		}
 	}
@@ -98,11 +88,6 @@ public class LoginActivity extends Activity
 			finish();
 		}
 
-		// mLocationClient = new LocationClient(this, this, this);
-		// Location mCurrentLocation;
-		// mCurrentLocation = mLocationClient.getLastLocation();
-		// System.out.print("ITT VAGYUNK HĂ‰Ă‰Ă‰J!!!!!!!!!!!!!!: ");
-		// mCurrentLocation.toString();
 	}
 
 	@Override
@@ -115,30 +100,17 @@ public class LoginActivity extends Activity
 	ServiceConnection mConnection = new ServiceConnection() {
 
 		public void onServiceDisconnected(ComponentName name) {
-			Toast.makeText(LoginActivity.this, "Service is disconnected", 1000)
-					.show();
 			mBounded = false;
 			mGpsLocation = null;
 		}
 
 		public void onServiceConnected(ComponentName name, IBinder service) {
-			Toast.makeText(LoginActivity.this, "Service is connected", 1000)
-					.show();
 			mBounded = true;
 			LocalBinder mLocalBinder = (LocalBinder) service;
 			mGpsLocation = mLocalBinder.getServerInstance();
 
-			// InternetConnection.checkConnection(getApplicationContext(), new
-			// InternetConnectionContinue() {
-			//
-			// @Override
-			// public void onResume() {
-			// // TODO Auto-generated method stub
-			// locationTask.execute(new Boolean[] { true });
-			// }
-			// });
-			if (InternetConnection.isOnline(getApplicationContext())) {
-				//locationTask.execute(new Boolean[] { true });
+			if (InternetConnection.isOnline(getApplicationContext()) && locationTask.getStatus() != AsyncTask.Status.PENDING ) {
+				locationTask.execute(new Boolean[] { true });
 			}
 
 		}
@@ -178,7 +150,6 @@ public class LoginActivity extends Activity
 				// Ha a nĂ©v Ă©s jelszĂł pĂˇros helyes
 				Log.e("LOGIN SCREEN",
 						"CORRECT USERNAME-PASSWORD PAIR, CLUBSACTIVITY STARTING");
-				// cityname = "Debrecen";
 				mGpsLocation.onDestroy();
 				locationTask.cancel(true);
 
@@ -198,27 +169,9 @@ public class LoginActivity extends Activity
 		}
 	}
 
-	/*
-	 * //EZ ĂŤGY SZAR!!!! NE HASZNĂ�LJA SENKI SEM! :) private String
-	 * getMyCityName() { List<Address> resultArray = null; Geocoder geoc = new
-	 * Geocoder(getApplicationContext()); try { resultArray =
-	 * geoc.getFromLocation(clubArray.get(i).getLatlng().latitude,
-	 * clubArray.get(i).getLatlng().longitude, 1); } catch (IOException e) {
-	 * e.printStackTrace(); } String resultAddress = ""; if(resultArray.size() >
-	 * 0) { resultAddress = resultArray.get(0).toString();
-	 * Log.e("GETMYCITYNAME", "ADDRESS ELEMENT 0 " + resultAddress[0]); for(int
-	 * j = 1; j < resultArray.get(i).toString()); Log.e("GETMYCITYNAME",
-	 * "ADDRESS ELEMENT " + j + ": " + resultAddress[j]); } return
-	 * resultAddress[0]; }
-	 */
 
 	void loginSynchronize(User actualUser) {
 		Session.setActualUser(actualUser);
-		// String cityname = "Debrecen";
-		// GPSLocation locator = new GPSLocation();
-		// while( ! locator.gotLocation() ) {
-		// }
-		// cityname = locator.getCityName();
 		Log.e("LOGIN SYNCHRONIZE", "LOGIN IN PROGRESS, NAME OF ACTUAL CITY: "
 				+ cityname);
 		actualUser.favoriteClubs = Session.getInstance()
@@ -250,127 +203,4 @@ public class LoginActivity extends Activity
 		return result;
 	}
 
-	/*
-	 * public static class ErrorDialogFragment extends DialogFragment {
-	 * 
-	 * // Global field to contain the error dialog private Dialog mDialog; //
-	 * Default constructor. Sets the dialog field to null public
-	 * ErrorDialogFragment() { super(); mDialog = null; } // Set the dialog to
-	 * display public void setDialog(Dialog dialog) { mDialog = dialog; } //
-	 * Return a Dialog to the DialogFragment.
-	 * 
-	 * @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
-	 * return mDialog; } }
-	 */
-	/*
-	 * @Override protected void onActivityResult( int requestCode, int
-	 * resultCode, Intent data) { // Decide what to do based on the original
-	 * request code switch (requestCode) { case
-	 * CONNECTION_FAILURE_RESOLUTION_REQUEST : // If the result code is
-	 * Activity.RESULT_OK, try to connect again
-	 * 
-	 * switch (resultCode) { case Activity.RESULT_OK : //Try the request again
-	 * 
-	 * break; } } }
-	 */
-	/*
-	 * private boolean servicesConnected() { // Check that Google Play services
-	 * is available int resultCode = GooglePlayServicesUtil.
-	 * isGooglePlayServicesAvailable(this); // If Google Play services is
-	 * available if (ConnectionResult.SUCCESS == resultCode) { // In debug mode,
-	 * log the status Log.d("Location Updates",
-	 * "Google Play services is available."); // Continue return true; // Google
-	 * Play services was not available for some reason } else { // Get the error
-	 * code ConnectionResult cr = null; int errorCode = cr.getErrorCode(); //
-	 * Get the error dialog from Google Play services Dialog errorDialog =
-	 * GooglePlayServicesUtil.getErrorDialog( errorCode, this,
-	 * CONNECTION_FAILURE_RESOLUTION_REQUEST);
-	 * 
-	 * // If Google Play services can provide an error dialog if (errorDialog !=
-	 * null) { // Create a new DialogFragment for the error dialog
-	 * ErrorDialogFragment errorFragment = new ErrorDialogFragment(); // Set the
-	 * dialog in the DialogFragment errorFragment.setDialog(errorDialog); //
-	 * Show the error dialog in the DialogFragment
-	 * errorFragment.show(getSupportFragmentManager(), "Location Updates");
-	 * 
-	 * } } return false; }
-	 */
-
-	/*
-	 * Called by Location Services if the attempt to Location Services fails.
-	 */
-	// @Override
-	// public void onConnectionFailed(ConnectionResult connectionResult) {
-	// /*
-	// * Google Play services can resolve some errors it detects.
-	// * If the error has a resolution, try sending an Intent to
-	// * start a Google Play services activity that can resolve
-	// * error.
-	// */
-	// if (connectionResult.hasResolution()) {
-	// try {
-	// // Start an Activity that tries to resolve the error
-	// connectionResult.startResolutionForResult(
-	// this,
-	// CONNECTION_FAILURE_RESOLUTION_REQUEST);
-	// /*
-	// * Thrown if Google Play services canceled the original
-	// * PendingIntent
-	// */
-	// } catch (IntentSender.SendIntentException e) {
-	// // Log the error
-	// e.printStackTrace();
-	// }
-	// } else {
-	// /*
-	// * If no resolution is available, display a dialog to the
-	// * user with the error.
-	// */
-	// showDialog(connectionResult.getErrorCode());
-	// }
-	// }
-
-	/*
-	 * Called by Location Services when the request to connect the client
-	 * finishes successfully. At this point, you can request the current
-	 * location or start periodic updates
-	 */
-	// @Override
-	// public void onConnected(Bundle dataBundle) {
-	// // Display the connection status
-	// Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-	//
-	// }
-
-	/*
-	 * Called by Location Services if the connection to the location client
-	 * drops because of an error.
-	 */
-	// @Override
-	// public void onDisconnected() {
-	// // Display the connection status
-	// Toast.makeText(this, "Disconnected. Please re-connect.",
-	// Toast.LENGTH_SHORT).show();
-	// }
-
-	/*
-	 * Called when the Activity becomes visible.
-	 */
-	// @Override
-	// protected void onStart() {
-	// super.onStart();
-	// // Connect the client.
-	// mLocationClient.connect();
-	// }
-
-	/*
-	 * Called when the Activity is no longer visible.
-	 */
-	// @Override
-	// protected void onStop() {
-	// // Disconnecting the client invalidates it.
-	// mLocationClient.disconnect();
-	// super.onStop();
-	// }
-	//
 }
