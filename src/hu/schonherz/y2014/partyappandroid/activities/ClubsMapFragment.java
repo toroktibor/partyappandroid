6599c6dc1a44c7aba64aca3f8271e90a37910644
kapizoli <kapi.zoli@freemail.hu>
2014-01-28 17:53:49 +0100

    Térkép pötty kattintás!?!? Nem tudni műxik-e... (Tibi)

--- src/hu/schonherz/y2014/partyappandroid/activities/ClubsMapFragment.java
+++ src/hu/schonherz/y2014/partyappandroid/activities/ClubsMapFragment.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -24,184 +27,258 @@ import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ClubsMapFragment extends Fragment implements ClubsUpdateableFragment {
+public class ClubsMapFragment extends Fragment implements
+		ClubsUpdateableFragment {
 
-    private GoogleMap googleMap;
-    private static View view;
-    List<MarkerOptions> markerList = new ArrayList<MarkerOptions>();
-    
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    if (view != null) {
-	    ViewGroup parent = (ViewGroup) view.getParent();
-	    if (parent != null)
-		parent.removeView(view);
-	}
-	try {
-	    view = inflater.inflate(R.layout.fragment_clubs_map, container, false);
-	} catch (InflateException e) {
-	    /* map is already there, just return view as it is */
-	}
-	initilizeMap();
-	updateResults();
-	return view;
-    }
+	private GoogleMap googleMap;
+	private static View view;
+	List<MarkerOptions> markerList = new ArrayList<MarkerOptions>();
 
-    @Override
-    public void updateResults() {
-	Log.i("MAP", "REFRESH RESULTS ON THE MAP");
-	if (googleMap != null) {
-		BitmapDescriptor bmd = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		if (view != null) {
+			ViewGroup parent = (ViewGroup) view.getParent();
+			if (parent != null)
+				parent.removeView(view);
+		}
+		try {
+			view = inflater.inflate(R.layout.fragment_clubs_map, container,
+					false);
+		} catch (InflateException e) {
+			/* map is already there, just return view as it is */
+		}
+		initilizeMap();
+		googleMap.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+
+			@Override
+			public void onInfoWindowClick(Marker arg0) {
+
+				final Activity activity = getActivity();
+				final Intent i = new Intent(activity, ClubActivity.class);
+				int listPosition = markerList.indexOf(arg0);
+				i.putExtra("listPosition", listPosition);
+				final Club actualClub = Session.getSearchViewClubs().get(
+						listPosition);
+
+				if (actualClub.isNotFullDownloaded()) {
+
+					Session.getInstance().progressDialog = ProgressDialog.show(
+							activity, "Kérlek várj", "Adatok betöltése...",
+							true, false);
+					new Thread(new Runnable() {
+
+						@Override
+						public void run() {
+							actualClub.downloadEverything();
+							activity.runOnUiThread(new Runnable() {
+
+								@Override
+								public void run() {
+									activity.startActivity(i);
+									activity.overridePendingTransition(
+											R.anim.slide_left_in,
+											R.anim.slide_left_out);
+									Session.getInstance()
+											.dismissProgressDialog();
+								}
+							});
+						}
+					}).start();
+				} else {
+					activity.startActivity(i);
+					activity.overridePendingTransition(R.anim.slide_left_in,
+							R.anim.slide_left_out);
+				}
+				return;
+			}
+		});
+
+		updateResults();
+		return view;
+	}
+
+	@Override
+	public void updateResults() {
+		Log.i("MAP", "REFRESH RESULTS ON THE MAP");
+		if (googleMap != null) {
+			BitmapDescriptor bmd = BitmapDescriptorFactory
+					.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET);
+			List<Address> addressList = new ArrayList<Address>();
+			Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+			LatLng actualClubsLatLng;
+
+			for (int i = 0; i < Session.getSearchViewClubs().size(); ++i) {
+				Club actualClub = Session.getSearchViewClubs().get(i);
+				try {
+					Log.e("MAP", actualClub.address);
+					addressList = geocoder.getFromLocationName(
+							actualClub.address, 1);
+					Log.e("MAP LATITUDE", ((Double) (addressList.get(0)
+							.getLatitude())).toString());
+					Log.e("MAP ADDRESSLIST: ",
+							geocoder.getFromLocationName(actualClub.address, 1)
+									.get(0).toString());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				if (addressList.size() > 0) {
+					actualClubsLatLng = new LatLng(addressList.get(0)
+							.getLatitude(), addressList.get(0).getLongitude());
+					if (actualClub.approved == 1) {
+						markerList.add(new MarkerOptions()
+								.position(actualClubsLatLng)
+								.title(actualClub.name)
+								.snippet(actualClub.address).icon(bmd));
+						googleMap
+								.addMarker(markerList.get(markerList.size() - 1));
+						Log.e("MAP", "NEW CLUB APPEARED ON THE MAP");
+					}
+				} else {
+					Log.e("MAP", "SIZE OF ADDRESSLIST IS 0");
+				}
+			}
+
+			if (markerList.size() > 0) {
+				LatLngBounds.Builder builder = new LatLngBounds.Builder();
+				for (int i = 0; i < markerList.size(); ++i) {
+					builder.include(markerList.get(i).getPosition());
+				}
+				LatLngBounds bounds = builder.build();
+				int padding = 0; // offset from edges of the map in pixels
+				CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds,
+						padding);
+				// googleMap.animateCamera(cu);
+			}
+			Log.e("MAP", "APPROVED PLACES SHOWN");
+		}
+	}
+
+	private void initilizeMap() {
+		if (googleMap == null) {
+			FragmentManager fragmentManager = getFragmentManager(); // lehetséges
+			// hibaforrás....
+			googleMap = ((SupportMapFragment) fragmentManager
+					.findFragmentById(R.id.mapFragment)).getMap();
+			googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+			googleMap.setMyLocationEnabled(true);
+			// check if map is created successfully or not
+			if (googleMap == null) {
+				Toast.makeText(getActivity(), "Sorry! Unable to create maps",
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+
+	private void showOnlyApprovedPlacesOnTheMap() {
+		BitmapDescriptor bmd = BitmapDescriptorFactory
+				.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET);
 		List<Address> addressList = new ArrayList<Address>();
 		Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
 		LatLng actualClubsLatLng;
-		
 
 		for (int i = 0; i < Session.getSearchViewClubs().size(); ++i) {
-		    Club actualClub = Session.getSearchViewClubs().get(i);
-		    try {
-			Log.e("MAP", actualClub.address);
-			addressList = geocoder.getFromLocationName(actualClub.address, 1);
-			Log.e("MAP LATITUDE", ((Double) (addressList.get(0).getLatitude())).toString());
-			Log.e("MAP ADDRESSLIST: ", geocoder.getFromLocationName(actualClub.address, 1).get(0).toString());
-		    } catch (IOException e) {
-			e.printStackTrace();
-		    }
-		    if (addressList.size() > 0) {
-			actualClubsLatLng = new LatLng(addressList.get(0).getLatitude(), addressList.get(0).getLongitude());
-			if (actualClub.approved == 1) {
-			    markerList.add(new MarkerOptions().position(actualClubsLatLng).title(actualClub.name)
-				    .snippet(actualClub.address).icon(bmd));
-			    googleMap.addMarker(markerList.get(markerList.size() - 1));
-			    Log.e("MAP", "NEW CLUB APPEARED ON THE MAP");
+			Club actualClub = Session.getSearchViewClubs().get(i);
+			try {
+				Log.e("MAP", actualClub.address);
+				addressList = geocoder.getFromLocationName(actualClub.address,
+						1);
+				Log.e("MAP LATITUDE", ((Double) (addressList.get(0)
+						.getLatitude())).toString());
+				// Log.e("MAP ADDRESSLIST: ",
+				// geocoder.getFromLocationName(actualClub.address,
+				// 1).get(0).toString());
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		    } else {
-			Log.e("MAP", "SIZE OF ADDRESSLIST IS 0");
-		    }
+			if (addressList.size() > 0) {
+				actualClubsLatLng = new LatLng(
+						addressList.get(0).getLatitude(), addressList.get(0)
+								.getLongitude());
+				if (actualClub.approved == 1) {
+					markerList.add(new MarkerOptions()
+							.position(actualClubsLatLng).title(actualClub.name)
+							.snippet(actualClub.address).icon(bmd));
+					googleMap.addMarker(markerList.get(markerList.size() - 1));
+					Log.e("MAP", "NEW CLUB APPEARED ON THE MAP");
+				}
+			} else {
+				Log.e("MAP", "SIZE OF ADDRESSLIST IS 0");
+			}
 		}
 
 		if (markerList.size() > 0) {
-		    LatLngBounds.Builder builder = new LatLngBounds.Builder();
-		    for (int i = 0; i < markerList.size(); ++i) {
-			builder.include(markerList.get(i).getPosition());
-		    }
-		    LatLngBounds bounds = builder.build();
-		    int padding = 0; // offset from edges of the map in pixels
-		    CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
-		    //googleMap.animateCamera(cu);
+			LatLngBounds.Builder builder = new LatLngBounds.Builder();
+			for (int i = 0; i < markerList.size(); ++i) {
+				builder.include(markerList.get(i).getPosition());
+			}
+			LatLngBounds bounds = builder.build();
+			int padding = 0; // offset from edges of the map in pixels
+			CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds,
+					padding);
+			googleMap.animateCamera(cu);
 		}
-		Log.e("MAP", "APPROVED PLACES SHOWN");
+		return;
 	}
-    }
 
-    private void initilizeMap() {
-	if (googleMap == null) {
-	    FragmentManager fragmentManager = getFragmentManager(); // lehetséges
-								    // hibaforrás....
-	    googleMap = ((SupportMapFragment) fragmentManager.findFragmentById(R.id.mapFragment)).getMap();
-	    googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-	    googleMap.setMyLocationEnabled(true);
-	    // check if map is created successfully or not
-	    if (googleMap == null) {
-		Toast.makeText(getActivity(), "Sorry! Unable to create maps", Toast.LENGTH_SHORT).show();
-	    }
-	}
-    }
+	private void showEveryPlacesOnTheMap() {
+		BitmapDescriptor bmd;
+		List<Address> addressList = new ArrayList<Address>();
+		Geocoder geocoder = new Geocoder(getActivity());
+		LatLng actualClubsLatLng;
+		for (Club actualClub : Session.getSearchViewClubs()) {
+			try {
+				addressList = geocoder.getFromLocationName(actualClub.address,
+						1);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			actualClubsLatLng = new LatLng(addressList.get(0).getLatitude(),
+					addressList.get(0).getLongitude());
+			if (actualClub.approved == 0)
+				bmd = BitmapDescriptorFactory
+						.defaultMarker(BitmapDescriptorFactory.HUE_RED);
+			else
+				bmd = BitmapDescriptorFactory
+						.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
 
-    private void showOnlyApprovedPlacesOnTheMap() {
-	BitmapDescriptor bmd = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET);
-	List<Address> addressList = new ArrayList<Address>();
-	Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
-	LatLng actualClubsLatLng;
-	
-
-	for (int i = 0; i < Session.getSearchViewClubs().size(); ++i) {
-	    Club actualClub = Session.getSearchViewClubs().get(i);
-	    try {
-		Log.e("MAP", actualClub.address);
-		addressList = geocoder.getFromLocationName(actualClub.address, 1);
-		Log.e("MAP LATITUDE", ((Double) (addressList.get(0).getLatitude())).toString());
-		Log.e("MAP ADDRESSLIST: ", geocoder.getFromLocationName(actualClub.address, 1).get(0).toString());
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
-	    if (addressList.size() > 0) {
-		actualClubsLatLng = new LatLng(addressList.get(0).getLatitude(), addressList.get(0).getLongitude());
-		if (actualClub.approved == 1) {
-		    markerList.add(new MarkerOptions().position(actualClubsLatLng).title(actualClub.name)
-			    .snippet(actualClub.address).icon(bmd));
-		    googleMap.addMarker(markerList.get(markerList.size() - 1));
-		    Log.e("MAP", "NEW CLUB APPEARED ON THE MAP");
+			googleMap.addMarker(new MarkerOptions().position(actualClubsLatLng)
+					.title(actualClub.name).snippet(actualClub.address)
+					.icon(bmd));
 		}
-	    } else {
-		Log.e("MAP", "SIZE OF ADDRESSLIST IS 0");
-	    }
+		return;
 	}
 
-	if (markerList.size() > 0) {
-	    LatLngBounds.Builder builder = new LatLngBounds.Builder();
-	    for (int i = 0; i < markerList.size(); ++i) {
-		builder.include(markerList.get(i).getPosition());
-	    }
-	    LatLngBounds bounds = builder.build();
-	    int padding = 0; // offset from edges of the map in pixels
-	    CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
-	    googleMap.animateCamera(cu);
+	@Override
+	public void onResume() {
+		super.onResume();
+		/*
+		 * initilizeMap(); updateResults();
+		 */
 	}
-	return;
-    }
 
-    private void showEveryPlacesOnTheMap() {
-	BitmapDescriptor bmd;
-	List<Address> addressList = new ArrayList<Address>();
-	Geocoder geocoder = new Geocoder(getActivity());
-	LatLng actualClubsLatLng;
-	for (Club actualClub : Session.getSearchViewClubs()) {
-	    try {
-		addressList = geocoder.getFromLocationName(actualClub.address, 1);
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
-	    actualClubsLatLng = new LatLng(addressList.get(0).getLatitude(), addressList.get(0).getLongitude());
-	    if (actualClub.approved == 0)
-		bmd = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED);
-	    else
-		bmd = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
-
-	    googleMap.addMarker(new MarkerOptions().position(actualClubsLatLng).title(actualClub.name)
-		    .snippet(actualClub.address).icon(bmd));
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		/*
+		 * initilizeMap(); updateResults();
+		 */
 	}
-	return;
-    }
 
-    @Override
-    public void onResume() {
-	super.onResume();
 	/*
-	 * initilizeMap(); updateResults();
+	 * @Override public void onDestroyView() { super.onDestroyView();
+	 * SupportMapFragment f = (SupportMapFragment) getFragmentManager()
+	 * .findFragmentById(R.id.mapFragment); if (f != null)
+	 * getFragmentManager().beginTransaction().remove(f).commit(); }
 	 */
-    }
-
-    @Override
-    public void onStart() {
-	// TODO Auto-generated method stub
-	super.onStart();
-	/*
-	 * initilizeMap(); updateResults();
-	 */
-    }
-
-    /*
-     * @Override public void onDestroyView() { super.onDestroyView();
-     * SupportMapFragment f = (SupportMapFragment) getFragmentManager()
-     * .findFragmentById(R.id.mapFragment); if (f != null)
-     * getFragmentManager().beginTransaction().remove(f).commit(); }
-     */
 }
