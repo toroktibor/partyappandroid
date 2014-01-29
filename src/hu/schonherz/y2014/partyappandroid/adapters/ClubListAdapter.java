6e040f12aed52052f3a6c074c7fe3789b2232347
vtms <vtamashun@gmail.com>
2014-01-29 20:29:54 +0100

    Orientáció és lista fix

--- src/hu/schonherz/y2014/partyappandroid/adapters/ClubListAdapter.java
+++ src/hu/schonherz/y2014/partyappandroid/adapters/ClubListAdapter.java
@@ -38,7 +38,7 @@ public class ClubListAdapter extends ArrayAdapter<Club> {
 			Log.i("itt", ((Club) getItem(position)).highlite_expire+" "+((Club) getItem(position)).name+" "+position);
 			item.setBackgroundColor(Color.YELLOW);
 		} else {
-			item.setBackgroundColor(Color.TRANSPARENT);
+			item.setBackgroundColor( getContext().getResources().getColor(R.color.applicationWindowBackground));
 		}
 	}
 	
