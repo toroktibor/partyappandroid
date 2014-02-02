337e34eac30c555b8f4dea633d51dac20ff6091b
vtms <vtamashun@gmail.com>
2014-02-02 21:46:36 +0100

    InfoToast

---
+++ src/hu/schonherz/y2014/partyappandroid/InfoToast.java
@@ -0,0 +1,42 @@
+package hu.schonherz.y2014.partyappandroid;
+
+import android.content.Context;
+import android.os.Vibrator;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class InfoToast {
+    private Context activity;
+    private String message;
+    public static Toast t = null;
+
+    public InfoToast(Context activity) {
+        this.activity = activity;
+    }
+
+    public InfoToast(Context activity, String message) {
+        this(activity);
+        this.message = message;
+    }
+
+    public void show() {
+        Vibrator vibrator = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(new long[] { 0, 50 }, -1); // egy rövid
+
+        LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // LayoutInflater inflater = (LayoutInflater)
+        // activity.getLayoutInflater();
+        View toastView = inflater.inflate(R.layout.toast_info, null);
+        TextView tv = (TextView) toastView.findViewById(R.id.toast_info_textview);
+        tv.setText(this.message);
+
+        t = new Toast(activity);
+        t.setGravity(Gravity.FILL_HORIZONTAL | Gravity.BOTTOM, 0, 0);
+        t.setDuration(Toast.LENGTH_SHORT);
+        t.setView(toastView);
+        t.show();
+    }
+}
